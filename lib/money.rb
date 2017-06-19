79ea2a584f324fc081932cee0de86b599f574926
André Furquim <andreccomputacao@gmail.com>
2017-06-19 17:15:45 -0300

    Conversion

--- lib/money.rb
+++ lib/money.rb
@@ -1,10 +1,14 @@
 class Money
+  class << self
+    attr_accessor :currency, :rates
+  end
+
   InvalidMoneyError = Class.new(StandardError)
 
   attr_accessor :amount, :currency
-  
+
   def initialize(amount, currency)
-    raise InvalidMoneyError, "Amount must be greater than 0" if amount <= 0
+    is_valid_amount?(amount)
     @amount = amount
     @currency = currency
   end
@@ -12,5 +16,30 @@ class Money
   def inspect
     return "#{@amount} #{currency}"
   end
+
+  def convert_to currency
+    # TODO: verificar se ta setado para o currency passado
+    # TODO: verificar se tem o que o cara quer converter
+    puts "#{self.amount}"
+    new_amount = self.amount * @@rates[currency]
+    return Money.new(new_amount.round(2), currency)
+  end
+
+  def self.conversion_rates(currency, rates={})
+    rates.values.each do |r|
+     raise InvalidMoneyError, "Amount must be greater than 0" if r <= 0
+    end
+
+    @@currency = currency
+    @@rates = rates
+    true
+  end
+
+  private
+
+    def is_valid_amount?(amount)
+      raise InvalidMoneyError, "Amount must be greater than 0" if amount <= 0
+    end
+
 end
 
--- test/money_spec.rb
+++ test/money_spec.rb
@@ -3,8 +3,9 @@ require 'conversion_dawanda'
 require 'money'
 
 describe Money do 
+  let(:fifty_eur) {Money.new(50, 'EUR')}
+
   it "is possible to create a money with a valid amount and a valid currency" do
-    fifty_eur = Money.new(50, 'EUR')
     fifty_eur.amount.must_equal 50
     fifty_eur.currency.must_equal 'EUR'
   end
@@ -15,7 +16,31 @@ describe Money do
   end
 
   it "is possible to inspect an instance to get the amount and currency" do
-    fifty_eur = Money.new(50, 'EUR')
     fifty_eur.inspect.must_equal "#{fifty_eur.amount} #{fifty_eur.currency}" 
   end
+
+  it "is possible to configure currency rates with respect to a base currency" do
+    conversion_rates = Money.conversion_rates('EUR', { 'USD' => 1.11, 'Bitcoin' => 0.0047 })
+    conversion_rates.must_equal true
+  end
+
+  it "is not possible to configure a currency rate with an invalid amount of currency" do
+    err = -> { Money.conversion_rates('EUR', { 'USD' => -1.11, 'Bitcoin' => -0.0047 })}.must_raise Money::InvalidMoneyError
+    err.message.must_match /Amount must be greater than 0/
+  end
+
+  it "is possible to convert to a different currency" do
+    Money.conversion_rates('EUR', { 'USD' => 1.11, 'Bitcoin' => 0.0047})
+    fifty_eur.convert_to('USD').amount.must_equal 55.50
+    fifty_eur.convert_to('USD').currency.must_equal 'USD'
+  end
+
+  it "should return a Money instance when convert is called" do
+    Money.conversion_rates('EUR', { 'USD'=> 1.11, 'Bitcoin' => 0.0047})
+    fifty_eur.convert_to('USD').must_be_instance_of Money
+  end
+
+  it "is not possible to convert an invalid amount" do
+  end
+
 end
